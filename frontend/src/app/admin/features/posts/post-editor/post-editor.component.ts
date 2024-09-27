c33820775d0bfc7fc84ba705c7586b456d577678
matashaEs <natali.babinska@ukr.net>
2024-09-27 16:10:07 +0200

    project structure frontend

---
+++ frontend/src/app/admin/features/posts/post-editor/post-editor.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post-editor',
+  standalone: true,
+  imports: [],
+  templateUrl: './post-editor.component.html',
+  styleUrl: './post-editor.component.scss'
+})
+export class PostEditorComponent {
+
+}
---
+++ frontend/src/app/admin/features/tags/tags-list/tags-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tags-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './tags-list.component.html',
+  styleUrl: './tags-list.component.scss'
+})
+export class TagsListComponent {
+
+}
