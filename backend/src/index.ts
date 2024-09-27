86c9440472a18e33806c111d26d89bc801803fbb
matashaEs <natali.babinska@ukr.net>
2024-09-27 11:53:38 +0200

    error logger

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,14 +3,16 @@ dotenv.config({
     path: './src/.env'
 });
 
-import express from 'express';
-
+import express, { Request, Response } from 'express';
+import 'express-async-errors';
 import './database/index';
 import categoryRoutes from './routes/category.routes';
 import tagRoutes from './routes/tag.routes';
 import postRoutes from './routes/post.routes';
 import commentRoutes from './routes/comment.routes';
 import authRoutes from './routes/auth.routes';
+import logger from './shared/logger.util';
+import { Category } from './models/Category';
 
 
 const app = express();
@@ -24,6 +26,15 @@ app.use('/api/tags', tagRoutes);
 app.use('/api/posts', postRoutes);
 app.use('/api/comments', commentRoutes);
 app.use('/api/auth', authRoutes);
+
+app.use((err: Error, req: Request, res: Response, next: any) => {
+    logger.error({
+        message: err.message,
+        stack: err.stack
+    })
+
+    res.status(500).send('Something went wrong');
+});
 
 app.listen(port, () => {
     console.log(`Server is running on port http://localhost:${port}`);
