9ea7a6b31dec189c22f51edd5c540f8d3c82f531
matashaEs <natali.babinska@ukr.net>
2024-09-30 12:44:33 +0200

    post detail frontend

--- frontend/src/app/public/features/posts/posts-list/posts-list.component.ts
+++ frontend/src/app/public/features/posts/posts-list/posts-list.component.ts
@@ -1,12 +1,40 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-posts-list',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './posts-list.component.html',
   styleUrl: './posts-list.component.scss'
 })
 export class PostsListComponent {
-
+  posts = [
+    {
+      id: 1,
+      title: 'Post 1',
+      content: "Content 1",
+      createdAt: new Date()
+    },
+    {
+      id: 2,
+      title: 'Post 2',
+      content: "Content 2",
+      createdAt: new Date()
+    },
+    {
+      id: 3,
+      title: 'Post 3',
+      content: "Content 3",
+      createdAt: new Date()
+    },
+    {
+      id: 4,
+      title: 'Post 4',
+      content: "Content 4",
+      createdAt: new Date()
+    }
+  ]
 }
