989b8f22d86121610a669d5fd2fb24aed8601127
matashaEs <natali.babinska@ukr.net>
2024-10-09 15:35:12 +0200

    guard admin

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,7 +1,10 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './core/guards/auth.guard';
 
 export const routes: Routes = [
     {path: '', loadChildren:()=> import('./public/public.module').then(m => m.PublicModule)},
-    {path: 'admin', loadChildren:()=> import('./admin/admin.module').then(m => m.AdminModule)},
+    {path: 'admin', 
+    canActivate: [authGuard], 
+    loadChildren:()=> import('./admin/admin.module').then(m => m.AdminModule)},
     {path: 'auth',loadChildren: () => import('./public/features/auth/auth.module').then((m) => m.AuthModule)}
 ];
